20c5d5723ea07764fa04708530d3713dae4dcfb9
godaeun <gowooda@naver.com>
2014-09-11 16:06:20 +0900

    ghg

--- loop.js
+++ loop.js
@@ -1,25 +1,23 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
   noFill();
 
   stroke(255, 0, 0);
 
-/*
-  var x;
-
-  x = 100;
-  ellipse(x, 100, 50, 50);
-
-  x = 250;
-  ellipse(x, 100, 50, 50);
-
-  x = 300;
-  ellipse(x, 100, 50, 50);
-*/
-
- for (var x = 1; x <=3; x++)
-{
-  ellipse(x * 70, x*20 + 100, 50, 50);
 }
+
+function draw () {
+
+   background(128, 0, 64);
+
+for (var x = 1; x <= 8; x++)
+{
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
+}
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
 }
