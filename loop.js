764cab6c130e000a9552c18cc168fa046cf3f648
godaeun <gowooda@naver.com>
2014-09-11 15:38:06 +0900

    dscfs

--- loop.js
+++ loop.js
@@ -5,6 +5,19 @@ function setup() {
 
   stroke(255, 0, 0);
 
+/*
+  var x;
+
+  x = 100;
+  ellipse(x, 100, 50, 50);
+
+  x = 250;
+  ellipse(x, 100, 50, 50);
+
+  x = 300;
+  ellipse(x, 100, 50, 50);
+*/
+
  for (var x = 1; x <=3; x++)
 {
   ellipse(x * 100, 100, 50, 50);
