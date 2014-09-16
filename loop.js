961da013a9f8861699508a8ff978e00f56c102d0
godaeun <gowooda@naver.com>
2014-09-17 01:11:47 +0900

    ff

--- loop.js
+++ loop.js
@@ -3,17 +3,17 @@ function setup() {
   
   noFill();
 
-  stroke(255, 0, 0);
+  stroke(150, 0, 0);
 
 }
 
 function draw () {
 
-   background(128, 0, 64);
+   background(128, 0, 264);
 
-for (var x = 1; x <= 8; x++)
+for (var x = 1; x <= 7; x++)
 {
-  ellipse(x * 70, x * 20 + 100, x+50, x*10);
+  ellipse(x * 80, x * 40 + 50, x+50, x*30);
 }
 
 var size =    Math.abs(mouseX - 320);
